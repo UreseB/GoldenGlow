322418740af03b81443a3997a7d059e8cfae621d
UreseB <uresebytyci@student.uni-pr.edu>
2025-04-08 18:53:29 +0200

    changed something

--- functions/dbh.php
+++ functions/dbh.php
@@ -15,8 +15,8 @@ if($conn) {
 
     // $sql = "CREATE TABLE users(user_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
 
-    $sql = "INSERT INTO users (username, email, password) VALUES ('uresa', 'uresa@gmail.com', 'uresa123')";
-    $conn->execute_query($sql);
+    //$sql = "INSERT INTO users (username, email, password) VALUES ('uresa', 'uresa@gmail.com', 'uresa123')";
+    //$conn->execute_query($sql);
     $sql = "INSERT INTO users (username, email, password) VALUES ('fjolla', 'fjolla@gmail.com', 'fjollafjolla')";
     $conn->execute_query($sql);
 }
