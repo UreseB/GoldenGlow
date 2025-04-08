5328f94861a15741c833e36c0c74d7acf334f935
UreseB <uresebytyci@student.uni-pr.edu>
2025-04-08 18:06:24 +0200

    for database

---
+++ functions/dbh.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "goldenglow"; // i merr me xampp nese don
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if($conn) {
+    echo "Connected successfully";
+
+
+    // $sql = "CREATE TABLE users(user_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('uresa', 'uresa@gmail.com', 'uresa123')";
+    $conn->execute_query($sql);
+}
+else{
+    die("Connection failed: " . $conn->connect_error);
+}
